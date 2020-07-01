4d35c7298c4e31ad82060bf8184d3bcd26b9810d
Abdulhamit Akaslan <hamtaklsn@gmail.com>
2020-07-01 23:07:07 +0300

    accidenter

---
+++ accident-video-combiner.py
@@ -0,0 +1,50 @@
+import urllib.request
+from moviepy.editor import *
+from os import path
+import shutil
+videoPaths = []
+videos = []
+
+date = "202003"
+url = "http://www.trafik.gov.tr/kurumlar/trafik.gov.tr/09-Video/KGYS/"+date+"/"
+savePath = "D:\\Python\\VideoEditing\\accidents\\"
+
+def downloadVideos():
+    global videoPaths
+    if not os.path.exists(savePath):
+        os.mkdir(savePath)
+    for i in range(1,4):
+        name = str(i)+".mp4"
+        path = savePath+name
+        videoPaths.append(path)
+        videoUrl = url+name
+        if os.path.exists(path):
+            print("yes",name)
+        else:
+            print("no",name)
+            try:
+                urllib.request.urlretrieve(videoUrl,path)
+                print(name,"indi.")
+            except:
+                pass
+        
+
+def combineVideos():
+    global videos
+    for path in videoPaths:
+        try:
+            video = VideoFileClip(path)
+            video = video.cutout(video.duration-5,video.duration)
+            videos.append(video)
+        except:
+            pass
+    combinedVideo = concatenate_videoclips(videos)
+    combinedVideo.write_videofile(date+"Combined.mp4",codec="libx264")
+
+def deleteAccidentFolder():
+    if os.path.exists(savePath):
+        shutil.rmtree(savePath)
+
+deleteAccidentFolder()
+downloadVideos()
+combineVideos()
